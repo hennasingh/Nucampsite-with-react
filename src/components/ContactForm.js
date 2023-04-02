78435ae34aba66d2b5ad9f4c11b0987bae09e993
Henna Singh <hennasingh04@gmail.com>
2023-04-02 13:48:25 +0100

    Implemented minimal validation for name, phone and email in the contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,12 +1,14 @@
 import { Button, Label, Col, FormGroup } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateContactForm } from '../utils/validateContactForm';
 
 const ContactForm = () => {
     const handleSubmit = (values, { resetForm }) => {
         console.log('form values:', values);
         console.log('in JSON format:', JSON.stringify(values));
         resetForm();
-    }
+    };
+
     return (
         <Formik
             initialValues={{
@@ -19,6 +21,7 @@ const ContactForm = () => {
                 feedback:''
             }}
             onSubmit={handleSubmit}
+            validate={validateContactForm}
         >
             <Form>
                 <FormGroup row>
@@ -31,6 +34,10 @@ const ContactForm = () => {
                         placeholder='First Name'
                         className='form-control'
                         />
+                        <ErrorMessage name='firstName'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
 
@@ -44,6 +51,10 @@ const ContactForm = () => {
                         placeholder='Last Name'
                         className='form-control'
                     />
+                    <ErrorMessage name='lastName'>
+                        {(msg) => <p className='text-danger'>{msg}</p>}
+
+                     </ErrorMessage>
                 </Col>                    
                 </FormGroup>
 
@@ -57,6 +68,9 @@ const ContactForm = () => {
                         placeholder='Phone'
                         className='form-control'
                     />
+                    <ErrorMessage name='phoneNum'>
+                        {(msg) => <p className='text-danger'>{msg}</p>}
+                    </ErrorMessage>
                 </Col>                    
                 </FormGroup>
 
@@ -71,6 +85,10 @@ const ContactForm = () => {
                         type='email'
                         className='form-control'
                     />
+                    <ErrorMessage name='email'>
+                        {(msg) => <p className='text-danger'>{msg}</p>}
+
+                    </ErrorMessage>
                 </Col>                    
                 </FormGroup>
 
