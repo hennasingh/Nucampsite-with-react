e281c741ce2ff3873c25d7647a74df0cb9132b77
Henna Singh <hennasingh04@gmail.com>
2023-04-02 19:15:55 +0100

    Implemented commentForm using formik library, validation on rating and name and added the page to campsiteDetail

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,13 +1,27 @@
 import { useState } from 'react';
-import { Button, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { validateCommentForm } from '../../utils/validateCommentForm';
 
 const CommentForm = ({campsiteId}) => {
 
     const [modalOpen, setModalOpen] = useState(false);
 
+    const handleSubmit = (values) => {
+        const comment = {
+
+            campsiteId: parseInt(campsiteId),
+            rating: values.rating,
+            author: values.author,
+            text: values.commentText
+        };
+        console.log(comment);
+        setModalOpen(false);
+    };
+
     return (
         <>
-            <Button outline ='true' onClick ={() => setModalOpen(true)}>
+            <Button outline onClick ={() => setModalOpen(true)}>
                 <i className ='fa fa-pencil fa-lg' /> Add Comment
             </Button>
             <Modal isOpen={modalOpen}>
@@ -15,7 +29,60 @@ const CommentForm = ({campsiteId}) => {
                     Add Comment
                 </ModalHeader>
                 <ModalBody>
-                    campsite: {campsiteId}
+                    <Formik
+                        initialValues={{
+                            rating: undefined,
+                            author: '',
+                            commentText: ''
+                        }}
+                        onSubmit={handleSubmit}
+                        validate={validateCommentForm}
+                    >
+                        <Form>
+                            <FormGroup>
+                                <Label htmlFor='rating'>Rating</Label>
+                                <Field
+                                    name='rating'
+                                    as='select'
+                                    className='form-control'
+                                >
+                                    <option>Select...</option>
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Field>
+                                <ErrorMessage name='rating'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='author'>Your Name</Label>
+                                <Field
+                                    name='author'
+                                    placeholder='Your Name'
+                                    className='form-control'
+                                />
+                                <ErrorMessage name='author'>
+                                    {(msg) => <p className='text-danger'>{msg}</p>}
+                                </ErrorMessage>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor='commentText'>Comment</Label>
+                                <Field
+                                    name='commentText'
+                                    as='textarea'
+                                    rows='12'
+                                    className='form-control'
+                                />
+                            </FormGroup>
+                            <Button type='submit' color='primary'>
+                                Submit
+                            </Button>
+                        </Form>
+
+                    </Formik>
                 </ModalBody>
 
             </Modal>
